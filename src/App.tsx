ecfda0129ad03f1a038a46109c93f5eb8d39a7ae
ISC-EIbarra <EdsonP4100@gmail.com>
2024-05-14 16:42:15 -0700

    Added
    New Project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import BudgetForm from './components/BudgetForm';
+import { BudgetContext } from './context/BudgetContext';
+
+function App() {
+  const context = useContext(BudgetContext);
+
+  return (
+    <>
+      <header className="bg-blue-700 py-4 max-h-72">
+        <h1 className="text-center font-medium text-4xl text-white">
+          Planificador de Gastos
+        </h1>
+      </header>
+      <div className="max-w-3xl mx-auto bg-white rounded-3xl mt-10 p-10">
+        <BudgetForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,45 @@
+import { useMemo, useState } from 'react';
+
+function BudgetForm() {
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(parseInt(e.target.value));
+  };
+
+  const isValid = useMemo(() => {
+    return isNaN(budget) || budget <= 0;
+  }, [budget]);
+
+  return (
+    <form className="space-y-4">
+      <div className="flex flex-col space-y-4">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-medium text-center"
+        >
+          Definir Presupuesto
+        </label>
+        <input
+          id="number"
+          type="number"
+          className="w-full bg-white border border-gray-200 px-4 py-2 rounded-3xl"
+          placeholder="Define tú presupuesto"
+          name="budget"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex items-center justify-center">
+        <input
+          type="submit"
+          value="Definir Presupuesto"
+          className="px-4 py-2 rounded-3xl cursor-pointer bg-blue-600 hover:bg-blue-700 text-white hover:text-gray-100 font-medium disabled:opacity-40"
+          disabled={isValid}
+        />
+      </div>
+    </form>
+  );
+}
+
+export default BudgetForm;
